0a91e549efa1e5fc69bc33e942ce1ecec0b45688
al-gent <94gent@gmail.com>
2024-10-02 12:34:59 -0700

    format

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 
 st.title('Trivia Questions For Right Now')
 
+@st.cache_data
 def wiki_trending_today(n):
     """Grab n trending wikipedia articles from today
     Returns a tuple, (titles, extracts)"""
@@ -35,7 +36,7 @@ def wiki_trending_today(n):
             extracts.append(i['extract'])
     return titles, extracts
 
-
+@st.cache_data
 def find_corresponding_news(titles, n, m):
     """given a list of titles, collect news articles corresponding to the first n titles
     Returns a list of strings, where each string can be used as a context for LLM
@@ -84,38 +85,8 @@ res =generate_questions(extracts, news)
 for QA in res:
     if QA != 'pass':
         question, answer = QA.split('\nA: ')
-        st.text(question)
-        st.text(answer)
+        st.markdown(f"### Question:\n\n{question}")  # Larger font size for the question
+        with st.expander("Show Answer"):  # Expander to hide the answer
+            st.markdown(f"**Answer:**\n\n{answer}")  # Display the answer inside the expander
+        st.markdown("---")  
 
-
-
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-# @st.cache_data
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
-
-# data_load_state = st.text('Loading data...')
-# data = load_data(10000)
-# data_load_state.text("Done! (using st.cache_data)")
-
-# if st.checkbox('Show raw data'):
-#     st.subheader('Raw data')
-#     st.write(data)
-
-# st.subheader('Number of pickups by hour')
-# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# st.bar_chart(hist_values)
-
-# # Some number in the range 0-23
-# hour_to_filter = st.slider('hour', 0, 23, 17)
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-# st.map(filtered_data)
